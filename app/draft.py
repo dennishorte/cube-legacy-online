161bef4c04838fba560add8ede0c9987c92e3c4a
Dennis Horte <dennis@pinterest.com>
2020-10-02 21:18:46 -0700

    Models, migrations, and code for creating drafts

---
+++ app/draft.py
@@ -0,0 +1,78 @@
+import random
+
+from app import db
+from app.models import Card
+from app.models import Draft
+from app.models import Pack
+from app.models import PackCard
+from app.models import Participant
+from app.models import User
+
+
+def create_draft(
+        participants: list,  # List of usernames
+        pack_size: int,
+        num_packs: int,
+):
+    # Only create a new draft if there isn't already a draft in progress.
+    if any(map(lambda x: not x.complete, Draft.query.all())):
+        raise RuntimeError("Can't start a new draft while another draft is in progress.")
+    
+    draft = Draft(
+        complete=False,
+        pack_size=pack_size,
+        num_packs=num_packs,
+        num_seats=len(participants),
+    )
+    db.session.add(draft)
+
+    random.shuffle(participants)
+    for seat, p in enumerate(participants):
+        user = User.query.filter(User.username==p).first()
+        p_orm = Participant(
+            user=user,
+            draft=draft,
+            seat=seat,
+        )
+        db.session.add(p_orm)
+
+    for i, pack in enumerate(_make_packs(pack_size, num_packs, len(participants))):
+        pack_number = i % num_packs
+        seat_number = i / len(participants)
+        
+        pack_orm = Pack(
+            draft=draft,
+            seat_number=seat_number,
+            pack_number=pack_number,
+        )
+        db.session.add(pack_orm)
+
+        for card in pack:
+            pc = PackCard(
+                card=card,
+                draft=draft,
+                pack=pack_orm,
+            )
+            db.session.add(card)
+        
+    # db.session.commit()
+
+
+def _make_packs(pack_size, num_packs, num_players):
+    cards = Card.query.all()
+    total_cards = pack_size * num_packs * num_players
+    total_packs = num_packs * num_players
+
+    if len(cards) < total_cards:
+        raise ValueError("Not enough cards ({}) for {} packs of size {} and {} players.".format(
+            len(cards), num_packs, pack_size, num_players))
+
+    random.shuffle(cards)
+    cards = cards[:total_cards]
+    packs = []
+    for i in range(total_packs):
+        start = i * pack_size
+        end = start + pack_size
+        packs.append(cards[start:end])
+
+    return packs  # List of list of Card ORMs
--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class User(UserMixin, db.Model):
     
     cards_added = db.relationship('Card', backref='added_by')
     scars_applied = db.relationship('Scar', backref='added_by')
+    participations = db.relationship('Participant', backref='user')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -38,6 +39,7 @@ class Card(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     added_by_id= db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    picks = db.relationship('PackCard', backref='card')
     scars = db.relationship('Scar', backref='card')
 
     def __repr__(self):
@@ -48,9 +50,82 @@ class Scar(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     card_id = db.Column(db.Integer, db.ForeignKey('card.id'))
-    added_by_id= db.Column(db.Integer, db.ForeignKey('user.id'))
+    added_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     text=db.Column(db.String(256))
 
     def __repr__(self):
         return '<Scar {}>'.format(self.id)
 
+    
+class Draft(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    complete = db.Column(db.Boolean)
+    pack_size = db.Column(db.Integer)
+    num_packs = db.Column(db.Integer)
+    num_seats = db.Column(db.Integer)
+
+    packs = db.relationship('Pack', backref='draft')
+    pack_cards = db.relationship('PackCard', backref='draft')
+    participant = db.relationship('Participant', backref='draft')
+
+
+class Participant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    draft_id = db.Column(db.Integer, db.ForeignKey('draft.id'))
+    seat = db.Column(db.Integer)
+    
+    picks = db.relationship('PackCard', backref='picked_by')
+
+    def waiting_packs(self):
+        """
+        Return the set of all packs that are stacked up on this player in this draft,
+        ordered so that the first element return is the first pack that should be picked from.
+        """
+        all_packs = self.draft.packs
+        waiting = [x for x in all_packs if x.next_seat() == self.seat]
+        waiting.sort(key=self._pack_sort_key)
+        return waiting
+
+    @staticmethod
+    def _pack_sort_key(pack):
+        return (pack.pack_number, pack.num_picked)
+
+
+class Pack(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    draft_id = db.Column(db.Integer, db.ForeignKey('draft.id'))
+    seat_number = db.Column(db.Integer)
+    pack_number = db.Column(db.Integer)
+    num_picked = db.Column(db.Integer, default=0)
+
+    cards = db.relationship('PackCard', backref='pack')
+
+    def direction(self):
+        if self.pack_number % 2 == 0:
+            return 'left'    # Meaning if this is seat number 2, it will pass to seat 3.
+        else:
+            return 'right'
+
+    def next_seat(self):
+        num_seats = self.draft.num_seats
+
+        tmp = seat_number + num_seats * 100
+        if self.direction() == 'left':
+            tmp += num_picked
+        else:
+            tmp -= num_picked
+
+        return tmp % num_seats
+
+
+class PackCard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    card_id = db.Column(db.Integer, db.ForeignKey('card.id'))
+    draft_id = db.Column(db.Integer, db.ForeignKey('draft.id'))
+    pack_id = db.Column(db.Integer, db.ForeignKey('pack.id'))
+    picked_by_id = db.Column(db.Integer, db.ForeignKey('participant.id'))
+    pick_number = db.Column(db.Integer, default=-1)
+
+    def picked(self):
+        return self.pick_number > -1
