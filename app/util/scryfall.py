17467e10da65a8d9f028b276c147b2442fd21065
Dennis Horte <dennis@pinterest.com>
2020-10-10 17:34:47 -0700

    New method in scryfall utils for converting the json format

--- app/util/scryfall.py
+++ app/util/scryfall.py
@@ -1,5 +1,47 @@
 import requests
 import time
+from copy import deepcopy
+
+
+class CardConsts(object):
+    JSON_KEYS = (
+        # Meta data
+        'all_parts',
+        'card_faces',
+        'layout',
+        'object',
+
+        # Card data
+        'cmc',
+        'component',
+        'flavor_text',
+        'image_url',
+        'loyalty',
+        'mana_cost',
+        'name',
+        'oracle_text',
+        'power',
+        'toughness',
+        'type_line',
+    )
+
+    FACE_KEYS = (
+        'flavor_text',
+        'loyalty',
+        'mana_cost',
+        'name',
+        'object',
+        'oracle_text',
+        'power',
+        'toughness',
+        'type_line',
+    )
+
+    COMBINING_KEYS = (
+        'name',
+        'type_line',
+        'oracle_text',
+    )
 
 
 def fetch_one_from_scryfall(card_name):
@@ -27,3 +69,41 @@ def fetch_many_from_scryfall(card_names: list):
     print("...COMPLETE (fetching cards from Scryfall")
 
     return results
+
+
+def convert_to_clo_standard_json(card_json):
+    card_json = deepcopy(card_json)
+
+    _convert_to_face_array(card_json)
+    _convert_image_storage(card_json)
+    _convert_remove_unwanted_keys(card_json)
+
+    return card_json
+
+
+def _convert_to_face_array(card_json):
+    # If the card has no card_faces, make one out of the singleton face.
+    if not 'card_faces' in card_json:
+        face_json = {}
+        card_json['card_faces'] = face_json
+
+        for key in CardConsts.FACE_KEYS:
+            if key in card_json:
+                face_json[key] = card_json[key]
+
+            if key not in CardConsts.COMBINING_KEYS:
+                del card_json[key]
+
+
+def _convert_image_storage(card_json):
+    for face in card_json['card_faces']:
+        face['image_url'] = face['image_uris']['normal']
+        del face['image_uris']
+
+
+def _convert_remove_unwanted_keys(card_json):
+    all_parts = [card_json] + card_json['card_faces']
+    for part in all_parts:
+        for key in CardConsts.JSON_KEYS:
+            if key in part:
+                del part[key]
