849f06a3cb2c38173569b794b0477225307dea94
Dennis Horte <dennis@pinterest.com>
2020-11-02 12:11:15 -0800

    Profiling option in main.py

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from os import environ
 
 from app import app
 
+DO_PROFILING = False
+
+if DO_PROFILING:
+    from werkzeug.middleware.profiler import ProfilerMiddleware
+    app.config['PROFILE'] = True
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
+
 if __name__ == "__main__":
     port = int(environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
