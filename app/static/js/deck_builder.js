4ff5fa6c9ebdc38f5fd2c9b38487a0d8e34819f3
Dennis Horte <dennis@pinterest.com>
2020-11-15 19:24:55 -0800

    Put CSRF protection for ajax in basic.html

--- app/static/js/deck_builder.js
+++ app/static/js/deck_builder.js
@@ -108,16 +108,6 @@ $(document).ready(function() {
 
   // Save the deck
   $("#saveLink").click(function() {
-    let csrftoken = $('meta[name=csrf-token]').attr('content')
-
-    $.ajaxSetup({
-      beforeSend: function(xhr, settings) {
-        if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
-          xhr.setRequestHeader("X-CSRFToken", csrftoken)
-        }
-      }
-    })
-
     $.ajax({
       type: 'POST',
       url: $('#saveMeta').data('save-url'),
