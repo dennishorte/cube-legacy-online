949bfaa243c28a050e1b2a99dbe3af1809f3a9b7
Dennis Horte <dennis@pinterest.com>
2020-11-20 18:45:18 -0800

    Support large text blobs for game saves

--- app/models/game_models.py
+++ app/models/game_models.py
@@ -10,15 +10,16 @@ class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     state_json = db.Column(db.Text)
+    state_json_uni = db.Column(db.UnicodeText)
 
     @functools.cached_property
     def state(self):
-        return GameState(json.loads(self.state_json))
+        return GameState(json.loads(self.state_json_uni))
 
     def update(self, game_state):
         if hasattr(game_state, 'data'):
             game_state = game_state.data
 
-        self.state_json = json.dumps(game_state)
+        self.state_json_uni = json.dumps(game_state)
         db.session.add(self)
         db.session.commit()
