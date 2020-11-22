8a695467603fa2b63bcfa245a545312cb2ee0d1e
Dennis Horte <dennis@pinterest.com>
2020-11-21 16:34:25 -0800

    Start using mysql in dev instead of sqlite

--- app/config.py
+++ app/config.py
@@ -3,18 +3,18 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-if os.environ.get('FLASK_ENV') == 'development':
-    db_uri ="sqlite:///" + basedir + "/app.db"
-else:
-    PROD_DB_URI_STRING = "{driver}://{user}:{pw}@{host}:{port}/{db_name}"
-    db_uri = PROD_DB_URI_STRING.format(
-        driver="mysql+pymysql",
-        user=os.environ["DB_USER"],
-        pw=os.environ["DB_PASS"],
-        host=os.environ["DB_HOST"].split(':')[0],
-        port=os.environ["DB_HOST"].split(':')[1],
-        db_name=os.environ["DB_NAME"],
-    )
+# if os.environ.get('FLASK_ENV') == 'development':
+#     db_uri ="sqlite:///" + basedir + "/app.db"
+# else:
+DB_URI_STRING = "{driver}://{user}:{pw}@{host}:{port}/{db_name}"
+db_uri = DB_URI_STRING.format(
+    driver="mysql+pymysql",
+    user=os.environ["DB_USER"],
+    pw=os.environ["DB_PASS"],
+    host=os.environ["DB_HOST"].split(':')[0],
+    port=os.environ["DB_HOST"].split(':')[1],
+    db_name=os.environ["DB_NAME"],
+)
 
 class Config(object):
     COCKATRICE_FOLDER = 'static/cockatrice'
