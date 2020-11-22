268754de7198f2b817fb03d442f18b16d74a24bc
Dennis Horte <dennis@pinterest.com>
2020-11-21 16:40:52 -0800

    Convert game.state_json to use mediumtext

--- app/models/game_models.py
+++ app/models/game_models.py
@@ -2,6 +2,8 @@ import functools
 import json
 from datetime import datetime
 
+from sqlalchemy.dialects.mysql import MEDIUMTEXT
+
 from app import db
 from app.util.game_logic import GameState
 
@@ -9,17 +11,16 @@ from app.util.game_logic import GameState
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    state_json = db.Column(db.Text)
-    state_json_uni = db.Column(db.UnicodeText)
+    state_json = db.Column(MEDIUMTEXT)
 
     @functools.cached_property
     def state(self):
-        return GameState(json.loads(self.state_json_uni))
+        return GameState(json.loads(self.state_json))
 
     def update(self, game_state):
         if hasattr(game_state, 'data'):
             game_state = game_state.data
 
-        self.state_json_uni = json.dumps(game_state)
+        self.state_json = json.dumps(game_state)
         db.session.add(self)
         db.session.commit()
