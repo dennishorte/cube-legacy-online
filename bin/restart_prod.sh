3ed9de2d226e9c10fbca1561c49820768dec7ff9
Dennis Horte <dennis@pinterest.com>
2020-12-22 17:15:15 -0800

    Add some nice messages to restart script

--- bin/restart_prod.sh
+++ bin/restart_prod.sh
@@ -1,4 +1,13 @@
-bash bin/bundle_js.sh
-bash bin/apply_sass.sh
+echo "Restarting Cube Legacy Online"
+
+echo "...storing build time"
 echo $(date "+%s") >> app/build_time
+
+echo "...building js bundle"
+bash bin/bundle_js.sh
+
+echo "...preprocessing sass"
+bash bin/apply_sass.sh
+
+echo "...restarting apache"
 service apache2 restart
