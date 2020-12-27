3c84f793d487a560158683535bce5abb16e1c31b
Dennis Horte <dennis@pinterest.com>
2020-12-27 12:32:58 -0800

    Fix error in resource versioning
    Timestamps were being appended to the build_time file instead of replacing it.

--- bin/restart_prod.sh
+++ bin/restart_prod.sh
@@ -1,7 +1,7 @@
 echo "Restarting Cube Legacy Online"
 
 echo "...storing build time"
-echo $(date "+%s") >> app/build_time
+echo $(date "+%s") > app/build_time
 
 echo "...building js bundle"
 bash bin/bundle_js.sh
